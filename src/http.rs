154787f16f20a6648d39f021b193ee2df6557af1
Evhen <malysh.evgeniy@gmail.com>
2023-11-09 13:04:14 +0200

    pass 5th stage

--- src/http.rs
+++ src/http.rs
@@ -1,38 +1,118 @@
+use std::collections::HashMap;
 use std::fmt::Display;
-use tokio::io::{AsyncBufReadExt, BufReader, Result};
-use tokio::net::TcpStream;
+use tokio::io::{AsyncBufReadExt, Result};
 
 #[derive(Debug)]
 pub struct Request {
     pub method: Method,
     pub path: String,
     pub protocol: String,
+    pub headers: HashMap<String, String>,
+}
+
+impl Display for Request {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut resp_string = String::new();
+        resp_string.push_str(&format!(
+            "{} {} {}\r\n",
+            self.method.to_string(),
+            self.path.to_string(),
+            self.protocol,
+        ));
+        for (head_title, head_content) in &self.headers {
+            resp_string.push_str(&format!("{}: {}\r\n", head_title, head_content));
+        }
+        resp_string.push_str("\r\n");
+
+        return write!(f, "{}", resp_string);
+    }
 }
 
 impl Request {
-    pub fn new(method: Method, path: String) -> Self {
+    pub fn new(method: Method, path: String, headers: HashMap<String, String>) -> Self {
         return Self {
             method,
             path,
             protocol: "HTTP/1.1".to_string(),
+            headers,
         };
     }
 
-    pub async fn deserialize(reader: BufReader<&mut TcpStream>) -> Result<Box<Self>> {
-        let mut lines = reader.lines();
-        let header_iter = lines.next_line().await?.unwrap();
+    pub async fn deserialize(buf: &[u8]) -> Result<Box<Self>> {
+        println!("Parse request...");
+        println!("Incoming Buffer {:?}", buf);
+        
+        let mut lines = buf.lines();
 
-        let mut split_header = header_iter.split_whitespace();
-        let method = match split_header.next().unwrap() {
-            "GET" => Method::Get,
-            "POST" => Method::Post,
-            "PUT" => Method::Put,
-            "DELETE" => Method::Delete,
-            _ => panic!(),
+        let start_line = match lines.next_line().await? {
+            Some(start_line) => start_line,
+            None => panic!("Start Line was not read correctly."),
         };
-        let path = split_header.next().unwrap().to_string();
+        println!("Unparsed Start Line: {}", start_line);
 
-        return Ok(Box::new(Self::new(method, path)));
+        let mut split_start_line = start_line.split_whitespace();
+        let method = match split_start_line.next()
+         {
+            Some("GET") => Method::Get,
+            Some("POST") => Method::Post,
+            Some("PUT") => Method::Put,
+            Some("DELETE") => Method::Delete,
+            Some(_) => panic!("Method not supported."),
+            None => panic!("Method was not read correctly."),
+        };
+
+        println!("Request method is: {:?}", method);
+
+        let path = match split_start_line.next() {
+            Some(path) => path.to_string(),
+            None => panic!("Path was not read correctly."),
+        };
+
+        println!("Requested path: {}", path);
+        println!("{}", "=".repeat(40));
+        // println!("Unparsed request tail is: {:?}", &lines);
+
+        let mut headers = HashMap::new();
+
+        println!("Parse request Headers...");
+
+        loop {
+            let line = match lines.next_line().await? {
+                Some(l) => {
+                    if l.is_empty() {
+
+                        println!("Last header was parsed.");
+                        
+                        break;
+                    }
+
+                    println!("Parsed header: {}", &l);
+                    
+                    l
+                }
+                None => break,
+            };
+            
+            println!("Header: {}", line);
+
+            let mut split_header = line.split_whitespace();
+            let key = split_header.next().unwrap().to_string();
+            let value = split_header.next().unwrap().to_string();
+            headers.insert(key, value);
+        }
+
+        println!("{}", "=".repeat(40));
+        // println!("Unparsed request tail is: {:?}", &lines);
+
+        return Ok(Box::new(Self::new(method, path, headers)));
+    }
+
+    pub fn get_header_value(&self, header_name: &str) -> &str {
+        let header_value = match self.headers.get(header_name) {
+            Some(h) => h,
+            None => "",
+        };
+        return header_value;
     }
 }
 
@@ -46,13 +126,13 @@ pub struct Response {
 }
 
 impl Response {
-    pub fn ok(body: String) -> Self {
+    pub fn ok(body: &str) -> Self {
         return Self {
             protocol: "HTTP/1.1".to_string(),
             status: StatusCode::Ok,
             content_type: ContentType::TextPlain,
             content_len: body.len(),
-            body,
+            body: body.to_string(),
         };
     }
 
@@ -79,10 +159,7 @@ impl Display for Response {
             "Content-Type: {}\r\n",
             self.content_type.to_string(),
         ));
-        resp_string.push_str(&format!(
-            "Content-Length: {}\r\n",
-            self.content_len,
-        ));
+        resp_string.push_str(&format!("Content-Length: {}\r\n", self.content_len,));
         resp_string.push_str("\r\n");
         resp_string.push_str(&self.body);
 
@@ -128,13 +205,17 @@ impl Display for StatusCode {
 
 #[derive(Debug)]
 pub enum ContentType {
-    TextPlain
+    TextPlain,
 }
 
 impl Display for ContentType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        return write!(f, "{}", match self {
-            ContentType::TextPlain => "text/plain".to_string()
-        });
+        return write!(
+            f,
+            "{}",
+            match self {
+                ContentType::TextPlain => "text/plain".to_string(),
+            }
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use tokio::io::{AsyncWriteExt, BufReader, Result};
-use tokio::net::TcpListener;
 use crate::http::{Request, Response};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, Result};
+use tokio::net::TcpListener;
 
 mod http;
 
@@ -8,25 +8,47 @@ mod http;
 async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").await?;
 
-    loop {
-        let (mut stream, _) = listener.accept().await?;
-        println!("accepted new connection");
+    while let Ok((mut stream, _)) = listener.accept().await {
+        tokio::spawn(async move {
+            println!("accepted new connection");
 
-        let reader = BufReader::new(&mut stream);
+            let mut buf = [0; 512];
 
-        let request = Request::deserialize(reader).await?;
-        println!("{:?}", request);
+            loop {
+                match stream.read(&mut buf).await {
+                    Ok(b) => b,
+                    Err(_) => panic!("Could not read from stream."),
+                };
 
-        let path = request.path.as_str();
+                print!("Buffer {}", String::from_utf8_lossy(&buf[..]));
 
-        let response = if path.starts_with("/echo") {
-            Response::ok(path["/echo/".len()..].to_string())
-        } else if path.eq("/") {
-            Response::ok("".to_string())
-        } else {
-            Response::not_found()
-        };
+                let request = match Request::deserialize(&buf).await {
+                    Ok(request) => request,
+                    Err(e) => panic!("{}", e),
+                };
+                println!("Receive new request: {}", request.to_string());
 
-        stream.write(response.to_string().as_bytes()).await?;
+                let path = request.path.as_str();
+
+                let response = if path.starts_with("/echo") {
+                    Response::ok(&path["/echo/".len()..])
+                } else if path.eq("/") {
+                    Response::ok("")
+                } else if path.starts_with("/user-agent") {
+                    let body = request.get_header_value("User-Agent");
+                    println!("Value of the User-Agent header is: {}", body);
+
+                    Response::ok(body)
+                } else {
+                    Response::not_found()
+                };
+
+                match stream.write(response.to_string().as_bytes()).await {
+                    Ok(b) => b,
+                    Err(_) => panic!("Could not write the response to the stream."),
+                };
+            }
+        });
     }
+    Ok(())
 }

b99cb7505a2693194e1a0920a7699e2ff3a1b761
Evhen <malysh.evgeniy@gmail.com>
2023-11-10 13:28:01 +0200

    pass 8th stage

--- src/http.rs
+++ src/http.rs
@@ -1,5 +1,6 @@
-use std::collections::HashMap;
 use std::fmt::Display;
+use std::io::{Error, ErrorKind};
+use std::{collections::HashMap, fmt::Formatter};
 use tokio::io::{AsyncBufReadExt, Result};
 
 #[derive(Debug)]
@@ -8,10 +9,11 @@ pub struct Request {
     pub path: String,
     pub protocol: String,
     pub headers: HashMap<String, String>,
+    pub body: String,
 }
 
 impl Display for Request {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let mut resp_string = String::new();
         resp_string.push_str(&format!(
             "{} {} {}\r\n",
@@ -29,88 +31,114 @@ impl Display for Request {
 }
 
 impl Request {
-    pub fn new(method: Method, path: String, headers: HashMap<String, String>) -> Self {
+    pub fn new(
+        method: Method,
+        path: String,
+        headers: HashMap<String, String>,
+        body: String,
+    ) -> Self {
         return Self {
             method,
             path,
             protocol: "HTTP/1.1".to_string(),
             headers,
+            body,
         };
     }
 
     pub async fn deserialize(buf: &[u8]) -> Result<Box<Self>> {
         println!("Parse request...");
-        
+
         let mut lines = buf.lines();
 
-        let start_line = match lines.next_line().await? {
-            Some(start_line) => start_line,
-            None => panic!("Start Line was not read correctly."),
-        };
-        println!("Unparsed Start Line: {}", start_line);
-
-        let mut split_start_line = start_line.split_whitespace();
-        let method = match split_start_line.next()
-         {
-            Some("GET") => Method::Get,
-            Some("POST") => Method::Post,
-            Some("PUT") => Method::Put,
-            Some("DELETE") => Method::Delete,
-            Some(_) => panic!("Method not supported."),
-            None => panic!("Method was not read correctly."),
-        };
-
-        println!("Request method is: {:?}", method);
-
-        let path = match split_start_line.next() {
-            Some(path) => path.to_string(),
-            None => panic!("Path was not read correctly."),
-        };
-
-        println!("Requested path: {}", path);
-        println!("{}", "=".repeat(40));
-
+        let mut method = Method::Get;
+        let mut path = String::new();
         let mut headers = HashMap::new();
+        let mut body = String::new();
 
-        println!("Parse request Headers...");
+        while let Some(line) = lines.next_line().await? {
 
-        loop {
-            let line = match lines.next_line().await? {
-                Some(l) => {
-                    if l.is_empty() {
-
-                        println!("Last header was parsed.");
-                        
-                        break;
-                    }
-
-                    println!("Parsed header: {}", &l);
-                    
-                    l
-                }
-                None => break,
-            };
-            
-            println!("Header: {}", line);
-
-            let mut split_header = line.split_whitespace();
-            let key = split_header.next().unwrap().trim_end_matches(":").to_string();
-            let value = split_header.next().unwrap().to_string();
-            headers.insert(key, value);
+            if line.is_empty() {
+                continue;
+            } else if is_start_line(&line) {
+                (method, path) = parse_start_line(&line)?;
+                println!("Method: {:?}", &method);
+                println!("Path: {}", &path);
+                continue;
+            } else if is_header(&line) {
+                let (header_name, header_value) = parse_header(&line)?;
+                println!("Header: {}: {}", &header_name, &header_value);
+                headers.insert(header_name, header_value);
+                continue;
+            } else if !line.is_empty() {
+                body = line;
+                println!("Body: {}", &body);
+                break;
+            } else {
+                return Err(Error::new(ErrorKind::Other, "Invalid request"));
+            }
         }
+        let request = Request::new(method, path, headers, body);
+        println!("Parsed request: ");
+        println!("{}", request.to_string());
 
-        println!("{}", "=".repeat(40));
-
-        return Ok(Box::new(Self::new(method, path, headers)));
+        return Ok(Box::new(request));
     }
 
-    pub fn get_header_value(&self, header_name: &str) -> &str {
+    pub fn get_header_value(&self, header_name: &str) -> Option<&str> {
         let header_value = match self.headers.get(header_name) {
-            Some(h) => h,
-            None => "",
+            Some(h) => Some(h.as_str()),
+            None => None,
         };
         return header_value;
     }
+}
+
+fn parse_header(line: &str) -> Result<(String, String)> {
+    let mut split_header = line.split_whitespace();
+    let key = split_header
+        .next()
+        .unwrap()
+        .trim_end_matches(":")
+        .to_string();
+    let value = split_header.next().unwrap().to_string();
+
+    return Ok((key, value));
+}
+
+fn is_header(line: &str) -> bool {
+    line.contains(": ")
+}
+
+fn parse_start_line(line: &str) -> Result<(Method, String)> {
+    let mut split_start_line = line.split_whitespace();
+    let method = match split_start_line.next() {
+        Some("GET") => Method::Get,
+        Some("POST") => Method::Post,
+        Some("PUT") => Method::Put,
+        Some("DELETE") => Method::Delete,
+        Some(_) => return Err(Error::new(ErrorKind::Other, "Method not supported.")),
+        None => {
+            return Err(Error::new(
+                ErrorKind::Other,
+                "Method was not read correctly.",
+            ))
+        }
+    };
+
+    let path = match split_start_line.next() {
+        Some(path) => path.to_string(),
+        None => panic!("Path was not read correctly."),
+    };
+
+    return Ok((method, path));
+}
+
+pub fn is_start_line(line: &str) -> bool {
+    line.starts_with("GET")
+        || line.starts_with("POST")
+        || line.starts_with("PUT")
+        || line.starts_with("DELETE")
 }
 
 #[derive(Debug)]
@@ -133,6 +161,16 @@ impl Response {
         };
     }
 
+    pub fn created(content_type: ContentType) -> Self {
+        return Self {
+            protocol: "HTTP/1.1".to_string(),
+            status: StatusCode::Created,
+            content_type,
+            content_len: 0,
+            body: "".to_string(),
+        };
+    }
+
     pub fn not_found() -> Self {
         return Self {
             protocol: "HTTP/1.1".to_string(),
@@ -145,7 +183,7 @@ impl Response {
 }
 
 impl Display for Response {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let mut resp_string = String::new();
         resp_string.push_str(&format!(
             "{} {}\r\n",
@@ -173,7 +211,7 @@ pub enum Method {
 }
 
 impl Display for Method {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let str = match self {
             Method::Get => "GET".to_string(),
             Method::Put => "PUT".to_string(),
@@ -187,14 +225,16 @@ impl Display for Method {
 #[derive(Debug)]
 pub enum StatusCode {
     Ok,
+    Created,
     NotFound,
 }
 
 impl Display for StatusCode {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let str = match self {
-            StatusCode::Ok => "200 OK".to_string(),
-            StatusCode::NotFound => "404 Not Found".to_string(),
+            Self::Ok => "200 OK".to_string(),
+            Self::Created => "201 Created".to_string(),
+            Self::NotFound => "404 Not Found".to_string(),
         };
         return write!(f, "{}", str);
     }
@@ -207,7 +247,7 @@ pub enum ContentType {
 }
 
 impl Display for ContentType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         return write!(
             f,
             "{}",
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
+use std::io::{Error, ErrorKind};
 
-use crate::http::{Request, Response};
+use crate::http::{Method, Request, Response};
 use http::ContentType;
 use tokio::fs::File;
 use tokio::io::{AsyncReadExt, AsyncWriteExt, Result};
@@ -27,15 +28,7 @@ async fn main() -> Result<()> {
     return Ok(());
 }
 
-async fn read_file_content(directory_path: &str) -> Result<String> {
-    let mut file = File::open(directory_path).await?;
-    let mut file_content = String::new();
-    file.read_to_string(&mut file_content).await?;
-
-    return Ok(file_content);
-}
-
-async fn handle_connection(mut stream: TcpStream, directory_path: Option<String>) {
+async fn handle_connection(mut stream: TcpStream, directory_path: Option<String>) -> Result<()> {
     println!("accepted new connection");
 
     let mut buf = [0; 512];
@@ -45,51 +38,79 @@ async fn handle_connection(mut stream: TcpStream, directory_path: Option<String>
             break;
         }
 
-        print!("Buffer {}", String::from_utf8_lossy(&buf[..]));
-
-        let request = match Request::deserialize(&buf).await {
-            Ok(request) => request,
-            Err(e) => {
-                eprintln!("Error deserializing request: {}", e);
-                continue;
-            }
-        };
-        println!("Receive new request: {}", request.to_string());
+        let request = Request::deserialize(&buf).await?;
 
         let path = request.path.as_str();
-        let response = if path.starts_with("/echo") {
-            let dynamic_path = &path["/echo/".len()..];
-            Response::ok(dynamic_path, ContentType::TextPlain)
-        } else if path.eq("/") {
-            Response::ok("", ContentType::TextPlain)
+        let response: Response = if path.eq("/") {
+            get_simple_ok()
+        } else if path.starts_with("/echo") {
+            get_echo_response(&path)
         } else if path.starts_with("/user-agent") {
-            let user_agent = request.get_header_value("User-Agent");
-            println!("Value of the User-Agent header is: {}", user_agent);
-            Response::ok(user_agent, ContentType::TextPlain)
+            get_user_agent_response(&request)
         } else if path.starts_with("/files") {
-            let filename = &path["/files/".len()..];
-            let full_path = match directory_path {
-                Some(ref dir) => format!("{}/{}", dir, filename),
-                None => {
-                    println!("Directory does not exist.");
-                    continue
-                },
-            };
-            if let Ok(file_content) = read_file_content(&full_path).await {
-                Response::ok(&file_content, ContentType::OctetStream)
-            } else {
-                Response::not_found()
+            match request.method {
+                Method::Get => get_file_sent_response(&path, &directory_path).await?,
+                Method::Post => get_file_saved_response(&path, &directory_path, &request.body).await?,
+                _ => Response::not_found(),
             }
         } else {
             Response::not_found()
         };
-
-        if let Err(e) = stream.write(response.to_string().as_bytes()).await {
-            println!(
-                "Could not write the response to the stream with an Error: {}.",
-                e
-            );
-            break;
-        }
+        stream.write(response.to_string().as_bytes()).await?;
     }
+    Ok(())
+}
+
+async fn get_file_saved_response(path: &str, directory_path: &Option<String>, file_content: &str) -> Result<Response> {
+    let filename = &path["/files/".len()..];
+    let full_path = match directory_path {
+        Some(ref dir) => format!("{}/{}", dir, filename),
+        None => return Err(Error::new(ErrorKind::Other, "Directory does not exist.")),
+    };
+
+    let mut file = File::create(full_path).await?;
+    file.write_all(&file_content.as_bytes()).await?;
+
+    return Ok(Response::created(ContentType::OctetStream));
+}
+
+async fn get_file_sent_response(path: &str, directory_path: &Option<String>) -> Result<Response> {
+    let filename = &path["/files/".len()..];
+    let full_path = match directory_path {
+        Some(ref dir) => format!("{}/{}", dir, filename),
+        None => return Err(Error::new(ErrorKind::Other, "Directory does not exist.")),
+    };
+    if let Ok(file_content) = read_file_content(&full_path).await {
+        let response = Response::ok(&file_content, ContentType::OctetStream);
+        return Ok(response);
+    } else {
+        let response = Response::not_found();
+        return Ok(response);
+    }
+}
+
+async fn read_file_content(directory_path: &str) -> Result<String> {
+    let mut file = File::open(directory_path).await?;
+    let mut file_content = String::new();
+    file.read_to_string(&mut file_content).await?;
+
+    return Ok(file_content);
+}
+
+
+fn get_user_agent_response(request: &Request) -> Response {
+    if let Some(user_agent) = request.get_header_value("User-Agent") {
+        Response::ok(user_agent, ContentType::TextPlain)
+    } else {
+        Response::not_found()
+    }
+}
+
+fn get_echo_response(path: &str) -> Response {
+    let dynamic_path = &path["/echo/".len()..];
+    return Response::ok(dynamic_path, ContentType::TextPlain);
+}
+
+fn get_simple_ok() -> Response {
+    return Response::ok("", ContentType::TextPlain);
 }

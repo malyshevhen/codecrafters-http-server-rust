5a0aeabdf1d43117a324e8cbefb725e70846efce
Evhen <malysh.evgeniy@gmail.com>
2023-11-22 14:32:37 +0200

    Piece length and hashes

--- src/main.rs
+++ src/main.rs
@@ -128,7 +128,12 @@ fn main() -> anyhow::Result<()> {
             let mut hasher = Sha1::new();
             hasher.update(&info_encoded);
             let info_hash = hasher.finalize();
-            println!("Info Hash: {}", hex::encode(&info_hash));
+            println!("Info Hash: {}", hex::encode(info_hash));
+            println!("Piece Length: {}", t.info.plength);
+            println!("Piece Hashes:");
+            for hash in t.info.pieces.0 {
+                println!("{}", hex::encode(&hash));
+            }
         }
     }
 
